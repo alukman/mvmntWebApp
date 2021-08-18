0ac2f45675964cb8d7e0115c94f8557f899c5ff6
Andrew Lukman <alukman3@gatech.edu>
2021-08-17 23:45:10 -0400

    Initial commit

---
+++ src/main/java/org/gerejanewlife/mvmntWebApp/MvmntWebAppApplication.java
@@ -0,0 +1,13 @@
+package org.gerejanewlife.mvmntWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvmntWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvmntWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gerejanewlife/mvmntWebApp/MvmntWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gerejanewlife.mvmntWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvmntWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
